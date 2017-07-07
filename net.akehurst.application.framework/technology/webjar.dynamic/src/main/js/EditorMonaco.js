0812c180d558a920c9fc9c6d66412f9d97b67321
David Akehurst <david.akehurst@itemis.de>
2017-07-07 10:05:24 +0200

    Monaco Editor suport now has parsetree colouring and change event returns text content

--- net.akehurst.application.framework/technology/webjar.dynamic/src/main/js/EditorMonaco.js
+++ net.akehurst.application.framework/technology/webjar.dynamic/src/main/js/EditorMonaco.js
@@ -30,24 +30,53 @@ define([
 		let self = this
 		this.viewer = {}
 		this.parentEl = $('#'+parentId)
+		this.decorations = []
 		this.editor = monaco.editor.create(document.getElementById(parentId), {
 			value: initialContent
 		})
-
+		this.editor.onDidChangeModelContent(function(evt) {
+			$(self.parentEl).trigger( "oninput", evt )
+		})
 	}
 
-	Editor.prototype.getText = function() {
-//		try {
-//			return this.viewer.editor.getTextView().getText()
-//		} catch (err) {
-//			console.log("Error: "+err.message)
-			return ""
-//		}
+	Editor.prototype = {
+		getText : function() {
+			try {
+				return this.editor.getValue()
+			} catch (err) {
+				console.log("Error: "+err.message)
+			}
+		},
+	
+
+		createDecoration : function(start, end, title, cssClass) {
+			let range = {startLineNumber:start.lineNumber, startColumn:start.column, endLineNumber:end.lineNumber,endColumn:end.column}
+			let dec = { range:range, options: { inlineClassName: cssClass, hoverMessage:title } }
+			return dec
+		},
+		doNode : function(node, decors) {
+			if (node.isPattern) {
+				//can't use pattern name as a css-class
+			} else {
+				let start = this.editor.getModel().getPositionAt(node.start)
+				let end = this.editor.getModel().getPositionAt(node.start+node.length)
+				let dec = this.createDecoration(start, end, node.name, node.name.replace(/_/g,'-'))
+				decors.push(dec)
+			}
+			if (null != node.children) {
+				for(let i=0, len=node.children.length; i < len; i++) {
+					let child = node.children[i]
+					decors = this.doNode(child, decors)
+				}
+			}
+			return decors
+		},
+		updateParseTree : function(parseTree) {
+			//this.annotationModel.removeAnnotations('orion.annotation.error') //'parseTree')
+			let decors = this.doNode(parseTree, [])
+			this.decorations = this.editor.getModel().deltaDecorations(this.decorations, decors)
+		}
 	}
 	
-	Editor.prototype.updateParseTree = function(parseTree) {
-//		this.highlighter.update(parseTree)
-	}
-
 	return Editor
 })
